0d5fb64e6967b91db2940ce1ac2b7da40d811956
gusmorini <gustavommh@gmail.com>
2018-08-16 13:59:41 -0300

    atualizou a classe Usuario

--- index.php
+++ index.php
@@ -2,14 +2,25 @@
 
 	require_once('config.php');
 
-/*	$sql = new Sql();
+	
+	// $sql = new Sql();
+	// $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+	// echo json_encode($usuarios);
 
-	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+	// lista 1 usuário
+	// $teste = new Usuario();
+	// $teste->loadById(1);
+	// echo $teste;
 
-	echo json_encode($usuarios);*/
+	//lista todos os usuarios
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
 
-	$teste = new Usuario();
+	//Busca o usuario pelo login
+	//$search = Usuario::search('mar');
+	//echo json_encode($search);
 
-	$teste->loadById(1);
-
-	echo $teste;
+	//login, trazer os dados do usuario logado
+	$usuario = new Usuario();
+	$usuario->login("teste","12345");
+	echo $usuario;
