4ef524c1065834c6d8ddc366f5c11b8548c8da59
gusmorini <gustavommh@gmail.com>
2018-08-18 08:15:56 -0300

    atualizou classe ususario

--- class/Usuario.php
+++ class/Usuario.php
@@ -42,6 +42,14 @@ class Usuario
 
 	//-------------------------------------------------------------------------------
 
+	public function setData($data)
+	{
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro($data['dtcadastro']);
+	}
+
 	public static function getList()
 	{
 		$sql = new Sql();
@@ -66,11 +74,7 @@ class Usuario
 
 		if (count($results) > 0 )
 		{
-			$row = $results[0];
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro($row['dtcadastro']);
+			$this->setData($results[0]);
 		}
 		else
 		{
@@ -85,13 +89,24 @@ class Usuario
 
 		if (count($results) > 0 )
 		{
-			$row = $results[0];
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro($row['dtcadastro']);
+			$this->setData($results[0]);
 		}
 	}
+
+	public function insert2()
+	{
+		$sql = new Sql();
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+
+		if (count($results) > 0)
+		{
+			$this->setData($results[0]);
+		}
+	}
+
 
 	public function __toString()
 	{
